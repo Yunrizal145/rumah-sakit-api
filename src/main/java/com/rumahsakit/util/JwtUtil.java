0762850804d72db23ee9452edb338d9e72d6c5af
Yunrizal145 <hamad.yunrizal@gmail.com>
2022-09-24 14:30:05 +0700

    Create Generate Token

---
+++ src/main/java/com/rumahsakit/util/JwtUtil.java
@@ -0,0 +1,29 @@
+package com.rumahsakit.util;
+
+import com.rumahsakit.model.User;
+import io.smallrye.jwt.build.Jwt;
+
+import javax.inject.Singleton;
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Singleton
+public class JwtUtil {
+
+    public static String generateJwt(User user){
+
+        HashSet<String> roles = new HashSet<>(Arrays.asList("user", "admin"));
+
+        return Jwt.issuer("https://example.com/issuer")
+                .subject("Token")
+                .upn(user.getName())
+                .groups(roles)
+                .claim("name", user.getUsername())
+                .claim("password", user.getPassword())
+                .expiresAt(
+                        System.currentTimeMillis() + 3600
+                )
+                .sign();
+    }
+
+}
